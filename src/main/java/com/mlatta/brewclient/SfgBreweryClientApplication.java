69599d7afbeeb115d72fb8955f1b62aa05177b28
mlatta <martyn.latta@bipsolutions.com>
2020-05-26 16:55:49 +0100

    inital commit for brewery client

---
+++ src/main/java/com/mlatta/brewclient/SfgBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.mlatta.brewclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgBreweryClientApplication.class, args);
+	}
+
+}
